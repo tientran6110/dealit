56b2ed90aa74d58554dbdcd2150787af0684eb5b
Tien Tran <tientran6110@gmail.com>
2023-10-05 22:37:38 -0700

    fix bug 3

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,11 +1,12 @@
-import Product from "@/lib/models/product.models";
-import { connectToDB } from "@/lib/mongoose"
-import { scrapeAmazonProduct } from "@/lib/scrapper";
-import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
+
+import { getLowestPrice, getHighestPrice, getAveragePrice, getEmailNotifType } from "@/lib/utils";
+import { connectToDB } from "@/lib/mongoose";
+import Product from "@/lib/models/product.models";
+import { scrapeAmazonProduct } from "@/lib/scrapper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
-export const maxDuration = 10; // This function can run for a maximum of 300 seconds
+export const maxDuration = 300; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
